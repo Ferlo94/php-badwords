5e96cbb8d8f2ddc3cf4437e36306bb1326ee1ea3
Ferlo94 <100360746+Ferlo94@users.noreply.github.com>
2022-06-21 15:39:04 +0200

    frase non censurata stampata correttamente

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-
+    $testo = "Volevo andare al mare ma alla fine sono andato in chiesa";
+    $testo_censurato = "";
 ?>
 
 <!DOCTYPE html>
@@ -11,5 +12,20 @@
     <title>Document</title>
 </head>
 <body>
+
+<!-- 
+
+Descrizione:
+Creare una variabile con un paragrafo di testo a vostra scelta.
+Stampare a schermo il paragrafo e la sua lunghezza.
+Una parola da censurare viene passata dall'utente tramite parametro GET.
+Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare. 
+
+-->
+
+    <h1>
+        <?php echo $testo ?>
+    </h1>
+
 </body>
 </html>
